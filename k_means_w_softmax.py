dfdcfa78c149651057e948a8ab847690e6409cdb
sergio <ivanmota985@gmail.com>
2020-05-06 20:28:27 -0500

    version 1.0

--- k_means_w_softmax.py
+++ k_means_w_softmax.py
@@ -1,1 +1,8 @@
-sdvsdvdfvdfvdfvdfvdfvdfvdfvd
+#from scipy import special
+#from sklearn import cluster
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("cc_general.csv")
+
+print(df.shape)
